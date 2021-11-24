232a07e23de465f24b7987add896ff542a11d57c
Pavel <Pavel.Debchuk@gmail.com>
2021-11-24 19:56:10 +0300

    dz 1/3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,45 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items'
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// console.log(galleryItems);
+
+const galeryContainer = document.querySelector('.gallery');
+const cardGalery = createGalery('galery');
+galeryContainer.insertAdjacentHTML('beforeend', cardGalery);
+
+function createGalery (galery) {
+  return galleryItems.map(({preview, original, description}) => {
+    return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}"> 
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+        </a>
+      </li>
+    `;
+  })
+  .join('');
+  return markup;
+}
+
+const folsA = document.querySelector('.gallery');
+folsA.addEventListener("click", noLink);
+
+  function noLink(events) {
+    events.preventDefault();
+  };
+
+const listLi = document.querySelector('.gallery');
+listLi.style.listStyleType = "none";
+// let imgData;
+// const imgBig = document.querySelector('.gallery');
+// imgBig.addEventListener('click', imgOpen);
+//   function imgOpen (event) {
+//     return imgData = event.target.getAttribute('data-source');
+//   }
+
+
+  let gallery = new SimpleLightbox('.gallery a');
+  gallery.on('show.simplelightbox', function () {
+
+  });
